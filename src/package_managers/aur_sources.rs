5bb89c8cd56a0572de8c660ac590b4b1fd4dc46e
PyRo1121 <pyro1121@github.com>
2026-02-01 09:00:01 -0600

    style: apply rustfmt to aur_sources.rs

--- src/package_managers/aur_sources.rs
+++ src/package_managers/aur_sources.rs
@@ -257,9 +257,10 @@ async fn download_to_file(
 
     pb.finish_with_message(format!(
         "{} (done)",
-        dest_path
-            .file_name()
-            .map_or_else(|| dest_path.display().to_string(), |f| f.to_string_lossy().into_owned())
+        dest_path.file_name().map_or_else(
+            || dest_path.display().to_string(),
+            |f| f.to_string_lossy().into_owned()
+        )
     ));
     Ok(())
 }
